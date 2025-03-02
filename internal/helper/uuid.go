216cabc07efdcf486fdd5076ce18cf18086b9264
anilvpatel21 <anilpatel.iiitm@gmail.com>
2025-03-02 17:10:54 +0530

    exported generate id function

--- internal/helper/uuid.go
+++ internal/helper/uuid.go
@@ -3,6 +3,6 @@ package helper
 import "github.com/google/uuid"
 
 // Helper function to generate unique ID for Todo items (simplified).
-func generateID() string {
+func GenerateID() string {
 	return uuid.NewString()
 }
