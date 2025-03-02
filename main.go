0a2934f2f00e3ca1d8ab4291b79e13bc445d0b5d
anilvpatel21 <anilpatel.iiitm@gmail.com>
2025-03-02 17:18:51 +0530

    created server

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"example.com/todo-app/adapters/repository"
+	"example.com/todo-app/api"
+	internal "example.com/todo-app/internal/todo"
+)
+
+func main() {
+	repo := repository.NewInMemoryTodoRepository()
+	// Initialize the service with the repository
+	service := internal.NewService(repo)
+
+	router := api.SetupRoutes(service)
+
+	// Start the server
+	port := "8080"
+	fmt.Printf("Server starting on port %s...\n", port)
+	log.Fatal(router.Run(":" + port))
 }
